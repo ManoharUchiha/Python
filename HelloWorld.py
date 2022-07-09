9d23d074f4e67195f9848dc6db3e455843d18fa2
vanammanohar <98452634+ManoharUchiha@users.noreply.github.com>
2022-07-09 10:11:54 +0530

    Please approve this change

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,13 @@
-print('Hello World')
+
+stop = True
+while stop:
+    for i in range(1,100):
+        if i == 3:
+            stop = False
+            break
+        else:
+            print(i)
+
+
+for i in range(1,10):
+    print (i)
